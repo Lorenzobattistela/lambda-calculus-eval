fa535c517d62a87d52a415d1336d96d96cd85073
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-11-28 15:31:52 -0300

    [feat] parsing whole file

--- main.c
+++ main.c
@@ -3,8 +3,18 @@
 #include <stdio.h>
 
 int main(void) {
-  tokens_t parsed = parse_token("@");
-  p_print_token(parsed);
+  char token;
+  FILE *in = get_file("expr.lambda", "r");
+
+  while((token = next(in)) != (char) EOF && token != '\n') {
+    tokens_t parsed = parse_token(token);
+    if(parsed == ERROR) {
+      printf("\nSYNTAX ERROR: TOKEN NOT ALLOWED: %c\n", token);
+      return 1;
+    }
+    p_print_token(parsed);
+  }
+
   return 0;
 }
 
