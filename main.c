f75c2a64a07def92569715691d677e45d766c821
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-12-06 15:30:49 -0300

    testing deepcopying

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "io.h"
 #include "parser.h"
+#include "reducer.h"
 #include <stdio.h>
 
 void print_ast(struct AstNode *node);
@@ -9,6 +10,19 @@ int main(void) {
   
   struct AstNode *res = parse_expression(in, next(in)); 
   print_ast(res);
+  printf("\nTesting deepcopy: \n");
+  struct AstNode *res_2 = (struct AstNode *)malloc(sizeof(struct AstNode));
+  res_2->type = LAMBDA_EXPR;
+  res_2->node.lambda_expr = (struct LambdaExpression *)malloc(sizeof(struct LambdaExpression));
+  res_2->node.lambda_expr->parameter = 'c';
+
+  struct AstNode *body = (struct AstNode *)malloc(sizeof(struct AstNode));
+  body->type = VAR;
+  body->node.variable = (struct Variable *)malloc(sizeof(struct Variable));
+  body->node.variable->name = 'd';
+  res_2->node.lambda_expr->body = body;
+
+  print_ast(res_2);
   return 0;
 }
 
