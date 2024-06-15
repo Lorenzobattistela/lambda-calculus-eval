2075eea0ca8b6fb3ecdb0c2805b5e3088627b0c2
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-06-15 20:43:12 -0300

    testing new defs

--- main.c
+++ main.c
@@ -82,16 +82,21 @@ FILE *cli() {
 int main(void) {
   FILE *in = cli();
   HANDLE_NULL(in);
-
   HashTable *table = createHashTable();
 
-  AstNode *res = parse_expression(in, next(in));
+  AstNode *res = parse_expression(table, in, next(in));
   print_ast(res);
+  printf("\n");
+
+  expand_definitions(table, res);
+  print_ast(res);
+
   AstNode *red = reduce_ast(res);
   printf("\nReduced ast:\n");
   print_ast(red);
   printf("\n");
   free_ast(res);
+  destroyHashTable(table);
   close_file(in);
   return 1;
 }
