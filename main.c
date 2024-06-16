86c953ac7d7319d7c39811b5b581e462cb331cb3
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-06-16 19:34:46 -0300

    enhancing CLI and starting to develop configs

--- main.c
+++ main.c
@@ -6,6 +6,19 @@
 
 #define MAX_LAMBDA_LENGTH 1000
 #define TMP_PATH "tmp/expr.lambda"
+#define CONFIG_PATH "config"
+
+typedef enum {
+  APPLICATIVE, // always fully evaluate the arguments of a function before
+               // evaluating the function itself
+  NORMAL,      // reduction happens from the outside in
+} reduction_order_t;
+
+typedef struct {
+  FILE *file;
+  bool step_by_step_reduction;
+  reduction_order_t reduction_order;
+} Options;
 
 char *get_str_input(char *input_msg, char *error_msg) {
   char *input = malloc(MAX_LAMBDA_LENGTH + 1 * sizeof(char));
@@ -44,10 +57,52 @@ char *get_file_path_input() {
   return file_path;
 }
 
-FILE *cli() {
+bool get_reduction_step_option() {
+  int option;
+  while (true) {
+    printf("Choose: \n[1] Normal reduction\n[2] Step by step reduction\n");
+    scanf("%d", &option);
+    while (getchar() != '\n')
+      ;
+    switch (option) {
+    case 1: {
+      return false;
+    }
+    case 2: {
+      return true;
+    }
+    default:
+      return false;
+    }
+  }
+}
+
+reduction_order_t get_reduction_order() {
+  int option;
+  while (true) {
+    printf("Choose: \n[1] Normal order\n[2] Applicative Order\n");
+    scanf("%d", &option);
+    while (getchar() != '\n')
+      ;
+    switch (option) {
+    case 1: {
+      return NORMAL;
+    }
+    case 2: {
+      return APPLICATIVE;
+    }
+    }
+  }
+}
+
+// Options get_config_from_file() {}
+
+Options cli() {
   FILE *f;
   int option;
   bool valid_option = false;
+  bool step_by_step_reduction = get_reduction_step_option();
+  reduction_order_t reduction_order = get_reduction_order();
 
   while (!valid_option) {
 
@@ -76,11 +131,16 @@ FILE *cli() {
       break;
     }
   }
-  return f;
+  Options opt;
+  opt.file = f;
+  opt.step_by_step_reduction = step_by_step_reduction;
+  opt.reduction_order = reduction_order;
+  return opt;
 }
 
 int main(void) {
-  FILE *in = cli();
+  Options opt = cli();
+  FILE *in = opt.file;
   HANDLE_NULL(in);
   HashTable *table = createHashTable();
 
@@ -93,8 +153,9 @@ int main(void) {
 
   AstNode *red = reduce_ast(res);
   printf("\nReduced ast:\n");
-  print_ast(red);
-  printf("\n");
+  char *reduced_ast_str = ast_to_string(red);
+  printf("%s\n", reduced_ast_str);
+
   free_ast(res);
   destroyHashTable(table);
   close_file(in);
